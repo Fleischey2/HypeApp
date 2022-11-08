4cf5eed330089aa1025a457a9b66fbb0b99e4bf6
fleis <fleis@DESKTOP-L56U9AN>
2022-11-08 19:00:20 +0100

    Test Commit

---
+++ HypeApp/src/main/java/com/HypeWebApp/HypeApp/HypeAppApplication.java
@@ -0,0 +1,13 @@
+package com.HypeWebApp.HypeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypeAppApplication.class, args);
+	}
+
+}
---
+++ HypeApp/src/test/java/com/HypeWebApp/HypeApp/HypeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HypeWebApp.HypeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
